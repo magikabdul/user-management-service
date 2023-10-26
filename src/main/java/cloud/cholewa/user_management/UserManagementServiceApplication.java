ec565c76712a6dc999b53354023d9f0379ac177a
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2023-10-26 20:47:53 +0200

    initial commit

---
+++ src/main/java/cloud/cholewa/user_management/UserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.user_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/cholewa/user_management/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.user_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
