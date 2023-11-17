e5da10e6926c27e8feb95987b71bee70addad810
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2023-11-17 20:02:42 +0100

    UMA-05 changed to signed JWT token

--- src/main/java/cloud/cholewa/user_management/config/JwtTokenConfig.java
+++ src/main/java/cloud/cholewa/user_management/config/JwtTokenConfig.java
@@ -8,4 +8,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class JwtTokenConfig {
 
     String expiration;
+    String secret;
 }
--- src/main/java/cloud/cholewa/user_management/security/JwtTokenService.java
+++ src/main/java/cloud/cholewa/user_management/security/JwtTokenService.java
@@ -1,46 +1,56 @@
 package cloud.cholewa.user_management.security;
 
 import cloud.cholewa.user_management.config.JwtTokenConfig;
-import com.nimbusds.jose.JOSEObjectType;
-import com.nimbusds.jose.PlainHeader;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jwt.JWTClaimNames;
 import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.PlainJWT;
+import com.nimbusds.jwt.SignedJWT;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class JwtTokenService {
 
     private final JwtTokenConfig config;
 
+    @SneakyThrows
     public String generateToken(String login) {
-        PlainHeader header = new PlainHeader.Builder()
-                .type(JOSEObjectType.JWT)
+
+        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.HS256)
                 .build();
 
-        return new PlainJWT(
+        SignedJWT jwt = new SignedJWT(
                 header,
                 getJwtClaimSet(login, Instant.now(), Duration.ofMinutes(Integer.parseInt(config.getExpiration())))
-        ).serialize();
+        );
+
+        jwt.sign(new MACSigner(config.getSecret()));
+
+        return jwt.serialize();
     }
 
     private JWTClaimsSet getJwtClaimSet(
             final @NotNull String login,
             final Instant now,
             final Duration expiration
-            ) {
+    ) {
+
         return new JWTClaimsSet.Builder()
                 .subject("token")
                 .issuer("user mgmt service")
                 .claim("login", login)
-                .claim(JWTClaimNames.ISSUED_AT, now.toEpochMilli())
-                .claim(JWTClaimNames.EXPIRATION_TIME, now.toEpochMilli() + expiration.toMillis())
+                .claim(JWTClaimNames.ISSUED_AT, (now.toEpochMilli() / 1000))
+                .claim(JWTClaimNames.EXPIRATION_TIME, now.toEpochMilli() / 1000 + expiration.toSeconds())
                 .build();
     }
 }
