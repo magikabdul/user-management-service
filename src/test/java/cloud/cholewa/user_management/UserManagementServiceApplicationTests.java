bcb40730947e8e0fb12f53a20a0d80d56be85a89
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2024-01-04 16:04:27 +0100

    UMA-08 added profile

--- src/test/java/cloud/cholewa/user_management/UserManagementServiceApplicationTests.java
+++ src/test/java/cloud/cholewa/user_management/UserManagementServiceApplicationTests.java
@@ -2,8 +2,10 @@ package cloud.cholewa.user_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserManagementServiceApplicationTests {
 
     @Test
